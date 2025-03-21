6463c8deaed634737ba5112c16af952220cf656f
HyunWoo <mogs223@gmail.com>
2025-03-21 13:16:43 +0900

    동시진행

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println(" nice to meet you");
-        // 나는 주석
+        // 나는 주석이 아니었어
     }
 }
