99788c9ceec26d091e84aa2e06909da4a9ae1bd6
HyunWoo <mogs223@gmail.com>
2025-03-21 13:04:48 +0900

    배고프다

---
+++ Number.java
@@ -0,0 +1,22 @@
+public class Number {
+    int num1;
+    int num2;
+    int num3;
+    int num4;
+    int num5;
+    int num6;
+    int num7;
+    int num8;
+    int num9;
+    int num11;
+    int num12;
+    int num13;
+    int num14;
+    int num15;
+    int num16;
+    int num17;
+    int num18;
+    int num19;
+    int num20;
+    int num21;
+}
