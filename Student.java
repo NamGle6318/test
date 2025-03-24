4175300518972855cd4408418443918c4b8f0c38
HyunWoo <mogs223@gmail.com>
2025-03-24 10:01:58 +0900

    적절한 메시지2+인사말 추가2+Account추가

--- Student.java
+++ Student.java
@@ -2,5 +2,6 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Student h i");
         System.out.println("Student h i");
+        System.out.println("Student h i");
     }
 }
