11d25837491b3538c1344199eb13903053f95ca0
HyunWoo <mogs223@gmail.com>
2025-03-24 12:05:19 +0900

    Car print문 수정

--- Car.java
+++ Car.java
@@ -2,6 +2,10 @@ public class Car {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("짜잔");
+        System.out.println("hi");
+        System.out.println("짜잔");
+        System.out.println("hi");
+        System.out.println("짜잔");
 
     }
 }
